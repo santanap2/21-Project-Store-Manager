f139413476cdb71bece39ed8e5ad3e32db239184
PedroSantana <phsantana99@gmail.com>
2023-01-25 17:52:27 -0300

    requisito 6 finalizado, passou nos teste FINALMENTEEE

--- src/routers/salesRouter.js
+++ src/routers/salesRouter.js
@@ -3,14 +3,6 @@ const { createSales } = require('../controllers');
 
 const salesRouter = express.Router();
 
-salesRouter.get('/sales', (_req, res) => res.status(200).json({ message: '/SALES FUNCIONANDO' }));
-
-// salesRouter.post('/sales', async (req, res) => {
-//   const sale = req.body;
-//   const result = await salesModel.createSale(sale);
-//   return res.status(201).json(result);
-// });
-
 salesRouter.post('/sales', createSales);
 
 module.exports = salesRouter;
--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -3,6 +3,12 @@ const { productsModel } = require('../models');
 const { validateAll } = require('./validations/salesValidations');
 
 const createSale = async (sale) => {
+  const error = await validateAll(sale);
+  if (error.some((one) => one !== null)) {
+    const rightError = error.find((item) => item.message);
+    return rightError;
+  }
+
   const productExists = sale.map(async (item) => {
     const product = await productsModel.getProductById(item.productId);
     return product;
@@ -11,12 +17,6 @@ const createSale = async (sale) => {
 
   const notFound = resolvedProduct.some((one) => !one);
   if (notFound) return { message: 'Product not found' };
-
-  const error = await validateAll(sale);
-  if (error.some((one) => one !== null)) {
-    const rightError = error.find((item) => item.message);
-    return rightError;
-  }
 
   const result = await salesModel.createSale(sale);
   return result;
